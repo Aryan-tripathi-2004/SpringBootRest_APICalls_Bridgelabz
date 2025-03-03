a4453014462efe64bdc1e17cd6109c5abd4fdc76
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-03 16:12:13 +0530

    [Om Datt Tripathi] Use GET Request Method and pass name as query parameter

--- src/main/java/com/example/RestAPICalls/controller/RestControllers.java
+++ src/main/java/com/example/RestAPICalls/controller/RestControllers.java
@@ -1,6 +1,7 @@
 package com.example.RestAPICalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class RestControllers {
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Bridgelabz!";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
     }
 }
